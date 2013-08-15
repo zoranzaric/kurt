13c3cbc121ca9d53691e61366dd5430e51396a9d
Zoran Zaric <zz@zoranzaric.de>
2013-08-15 21:48:43 +0200

    Add WvTest
    Signed-off-by: Zoran Zaric <zz@zoranzaric.de>

---
+++ wvtest-kurt.sh
@@ -0,0 +1,24 @@
+# Include in your test script like this:
+#
+#   #!/usr/bin/env bash
+#   . ./wvtest-kurt.sh
+
+. ./wvtest.sh
+
+_wvtop="$(pwd)"
+
+wvmktempdir ()
+(
+    script_name="$(basename $0)"
+    set -e -o pipefail
+    mkdir -p "$_wvtop/t/tmp"
+    mktemp -d "$_wvtop/t/tmp/$script_name-XXXXXXX"
+)
+
+wvmkmountpt ()
+(
+    script_name="$(basename $0)"
+    set -e -o pipefail
+    mkdir -p "$_wvtop/t/mnt"
+    mktemp -d "$_wvtop/t/mnt/$script_name-XXXXXXX"
+)
